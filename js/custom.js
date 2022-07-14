ae67af2a5fe1d5e992d6716bdcda61270533b1a6
Babin Rana <babin.rana@javra.com>
2022-07-14 08:53:18 +0545

    Class 10

--- js/custom.js
+++ js/custom.js
@@ -48,3 +48,96 @@ function name() {
     // statement
 }
 */
+
+
+
+
+// var and let scope
+
+var globalVariable = "You can use me throughout js execution.";
+
+function test() {
+    let scopedVariable = "You can use me only in this function."
+}
+
+
+/**
+ * Operator
+ * 
+ * = for assign
+ * == for checking equal value
+ * === for checking equal value with same data type
+ * != for checking both value are not equal
+ * !== for checking both value are not equal with the same data type
+*/
+
+/**
+ * Array and Object
+ */
+var MyFirstArray = [1, 2, 3];
+
+console.log(MyFirstArray); // Display All data of array
+console.log(MyFirstArray[1]); // Display the data of Array index 1
+
+var MyFirstObject = {"kona" : { color: "red", model: "EV", seatCapacity: 4, battery: { model: "P200", maxCapacity: "1000 KW"} } };
+
+console.log(MyFirstObject); // Display the whole object's properties
+console.log(MyFirstObject.model); //Display which property you need to access.
+
+var person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 1234,
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+var carName = "Toyota \n Wow";
+
+// Get string length
+carName.length;
+
+//Slice
+let str = "Apple, Banana, Kiwi";
+let part = str.slice(7, 13); //Banana
+
+//String Replace
+part.replace("Banana", "orange");
+
+// To upper and to lower function
+part.toUpperCase();
+part.toLowerCase();
+
+// Joint two string or more.
+let text1 = "Hello";
+let text2 = "World";
+let text3 = text1.concat( " ", text2);
+let text4 = text1 + ' ' + text2;
+
+// trim
+let whiteSpacedWord = "             Hello World             ";
+console.log(whiteSpacedWord.trim());
+
+//Padstart padend
+let mo = "36";
+console.log(mo.padStart(10, "X"));
+
+let mo2 = "98";
+console.log(mo2.padEnd(10, "X"));
+
+//charAt
+let charWord = "lorem ipsum";
+console.log(charWord.charAt(0));
+
+//split
+
+let WholeName = "Jhon Doe";
+console.log(WholeName.split(" "));
+
+//indexOf, search and charAt
+
+let word = "Please Locate me in this string.";
+console.log( word.indexOf("me"));
+
+console.log( word.search("me"));
